f28112a4272135d2ee2b2803c1312cb80e5fa223
Colella Dimitri <dimitri.colella@edufr.ch>
2025-09-30 16:39:52 +0200

    try

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,15 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-    plugins: [vue()],
-    server: {
-        port: 8080,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8081',
-                changeOrigin: true
+export default defineConfig(({ command, mode }) => {
+    const base = '/'
+
+    return {
+        base,
+        server: {
+            port: 8080,
+            strictPort: true,
+            proxy: {
+                [`${base}api`]: {
+                    target: 'http://localhost:8081',
+                    changeOrigin: true,
+                }
             }
-        }
+        },
+        plugins: [vue()]
     }
 })
